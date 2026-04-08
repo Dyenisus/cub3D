9413270ca461c6421143823565d01403da45f9b1
Yekta Soytürk <github@inertia.slmail.me>
2026-04-08 19:55:21 +0300

    build_map_from_parser implemented

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: yesoytur <yesoytur@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/04 13:17:54 by yesoytur          #+#    #+#             */
-/*   Updated: 2026/04/05 17:04:55 by yesoytur         ###   ########.fr       */
+/*   Updated: 2026/04/08 19:54:21 by yesoytur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,25 @@ static t_map	*parse_fail(t_parser *parser)
 {
 	free_parser(parser);
 	return (NULL);
+}
+
+static t_map *buidl_map_from_parser(t_parser *parser)
+{
+	t_map	*map;
+
+	map = ft_calloc(1, sizeof(t_map));
+	if (!map)
+	{
+		print_error("Malloc failed");
+		return (NULL);
+	}
+	*map = parser->map;
+	parser->map.grid = NULL;
+	parser->map.north.path = NULL;
+	parser->map.south.path = NULL;
+	parser->map.west.path = NULL;
+	parser->map.east.path = NULL;
+	return (map);
 }
 
 t_map	*parse(char *path)
